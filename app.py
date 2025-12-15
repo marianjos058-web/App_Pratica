4978fc5175c65ea3dd5d431956eadd3f32d6881e
marianjos058-web <marianjos058@gmail.com>
2025-12-15 13:24:31 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.header("Introduzindo os Elementos do Streamlit")
 
