ae97658147a5ab75f4d837fc9466f7c30a855820
marianjos058-web <marianjos058@gmail.com>
2025-12-15 12:32:22 -0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with st. sidebar:
   if dados:
     def carregar_dados(dados):
       try:
-        df = pd. read_excel (dados)
+        df = pd.read_excel(dados)
         return df
       except FileNotFoundError:
         return pd.DataFrame()
