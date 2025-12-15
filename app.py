3c57fff759fee066d6a98279d75fd212d7bc79ce
marianjos058-web <marianjos058@gmail.com>
2025-12-15 11:16:29 -0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 st.header("Introduzindo os Elementos do Streamlit")
 
-menu = option_menu (menu_title_"Menu",
+menu = option_menu (menu_title="Menu",
                  options=["Início", "Gráficos Estáticos", "Gráficos Dinâmicos", "Widgets", "Formulário"],
                  icons=["house", "bar-chart", "bar-chart-line", "toggles", ""bar-chart],
                  menu_icon="cast",
